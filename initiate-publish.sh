7b70f08e7e659a1a118664576ed59316d1da5dd8
stefanGhivi <ghivi.stef@gmail.com>
2016-11-23 19:59:52 +0200

    travis deploy

--- initiate-publish.sh
+++ initiate-publish.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 if [ "$TRAVIS_REPO_SLUG" == "stefanGhivi/mangopay2-python-sdk" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "release" ]; then
 
   echo -e "Starting publish to PypiTest...\n"
